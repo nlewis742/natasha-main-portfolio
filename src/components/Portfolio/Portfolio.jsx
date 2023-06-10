37ab82781565e4b43f72ac0cf324e4122004c96d
Natasha <nlackas12@gmail.com>
2023-06-10 12:26:34 -0500

    added project links

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -8,9 +8,49 @@ function Portfolio() {
         <h1>Portfolio Component</h1>
 
         <article>
-        <div className="portfolio__item">
+        <div>
+          <a href="https://github.com/nlewis742/eat-speak-love" target="_blank" rel="noreferrer">
+
+      
             <h2>Project 1</h2>
             <p>Project 1 description</p>
+            </a>
+        </div>
+
+        <div>
+          <a href="https://github.com/nlewis742/birders-anonymous" target="_blank" rel="noreferrer">
+
+      
+            <h2>Project 2</h2>
+            <p>Project 1 description</p>
+            </a>
+        </div>
+
+        <div>
+          <a href="https://github.com/nlewis742/text_editor_pwa" target="_blank" rel="noreferrer">
+
+      
+            <h2>Project 3</h2>
+            <p>Project 1 description</p>
+            </a>
+        </div>
+
+        <div >
+          <a href="https://github.com/nlewis742/employee_tracker" target="_blank" rel="noreferrer">
+
+      
+            <h2>Project 4</h2>
+            <p>Project 1 description</p>
+            </a>
+        </div>
+
+        <div>
+          <a href="https://github.com/nlewis742/weather-dash" target="_blank" rel="noreferrer">
+
+      
+            <h2>Project 5</h2>
+            <p>Project 1 description</p>
+            </a>
         </div>
 </article>
     </div>
